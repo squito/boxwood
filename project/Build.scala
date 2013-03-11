6af1991a263a459daf3d152b0ac5aef858d43c01
Imran Rashid <imran@quantifind.com>
2013-03-10 18:15:00 -0700

    add build files

---
+++ project/Build.scala
@@ -0,0 +1,41 @@
+import sbt._
+import Keys._
+
+object SparkBuild extends Build {
+  lazy val core = Project("core", file("core"), settings = coreSettings)
+
+  val qf = "http://repo.quantifind.com/content/repositories/"
+  def sharedSettings = Defaults.defaultSettings ++ Seq(
+    version := "0.1-SNAPSHOT",
+    scalaVersion := "2.9.1",
+    organization := "com.quantifind",
+    scalacOptions := Seq(/*"-deprecation",*/ "-unchecked", "-optimize"), // -deprecation is too noisy due to usage of old Hadoop API, enable it once that's no longer an issue
+    unmanagedJars in Compile <<= baseDirectory map { base => (base / "lib" ** "*.jar").classpath },
+    retrieveManaged := true,
+    transitiveClassifiers in Scope.GlobalScope := Seq("sources"),
+    //publishTo <<= baseDirectory { base => Some(Resolver.file("Local", base / "target" / "maven" asFile)(Patterns(true, Resolver.mavenStyleBasePattern))) },
+    publishTo <<= version {
+      (v: String) =>
+        Some("snapshots" at qf + "ext-snapshots")
+    },
+    libraryDependencies ++= Seq(
+      "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+    )
+  )
+
+  val slf4jVersion = "1.6.1"
+
+  def coreSettings = sharedSettings ++ Seq(
+    name := "Boxwood",
+    resolvers ++= Seq(
+      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+      "JBoss Repository" at "http://repository.jboss.org/nexus/content/repositories/releases/"
+    ),
+    libraryDependencies ++= Seq(
+      "log4j" % "log4j" % "1.2.16",
+      "org.slf4j" % "slf4j-api" % slf4jVersion,
+      "org.slf4j" % "slf4j-log4j12" % slf4jVersion
+    )
+  )
+
+}
