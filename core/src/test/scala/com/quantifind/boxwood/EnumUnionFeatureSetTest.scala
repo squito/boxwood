b95d3881cc8b3d82d043606cff9d12a6f2972d2d
Imran Rashid <imran@quantifind.com>
2013-03-11 17:28:33 -0700

    fix conditions in test

--- core/src/test/scala/com/quantifind/boxwood/EnumUnionFeatureSetTest.scala
+++ core/src/test/scala/com/quantifind/boxwood/EnumUnionFeatureSetTest.scala
@@ -11,13 +11,13 @@ class EnumUnionFeatureSetTest extends FunSuite with ShouldMatchers {
 
     val f2 = new BaseFeatureSet with A_B_FeatureSet with Gender
     f2.nFeatures should be (Union_A_B.nEnums + 2)
-    f2.enumStartIdx should be (0)
-    f2.genderStartIdx should be (2)
+    f2.genderStartIdx should be (0)
+    f2.enumStartIdx should be (2)
 
     val f3 = new BaseFeatureSet with Age with A_B_FeatureSet
     f3.nFeatures should be (Union_A_B.nEnums + 6)
-    f3.ageStartIdx should be (0)
-    f3.enumStartIdx should be (6)
+    f3.enumStartIdx should be (0)
+    f3.ageStartIdx should be (4)
   }
 }
 
