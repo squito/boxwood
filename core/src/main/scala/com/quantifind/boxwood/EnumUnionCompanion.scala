f4468ca4b152aabcbfa99e76a6ba1c8a00e1ee6a
ryanlecompte <lecompte@gmail.com>
2013-03-11 22:44:04 -0700

    move getIdx into common trait

--- core/src/main/scala/com/quantifind/boxwood/EnumUnionCompanion.scala
+++ core/src/main/scala/com/quantifind/boxwood/EnumUnionCompanion.scala
@@ -1,11 +1,13 @@
 package com.quantifind.boxwood
 
+trait Union_A_B[T <: Enum[T]]
+
 trait EnumUnionCompanion {
   val enumClasses: Seq[Class[_ <: Enum[_]]]
-  lazy val enumToIdx: Map[Enum[_],Int] = {
+  lazy val enumToIdx: Map[Enum[_], Int] = {
     var idx = 0
-    val tmp = collection.mutable.Map[Enum[_],Int]()
-    enumClasses.foreach{enumClass =>
+    val tmp = collection.mutable.Map[Enum[_], Int]()
+    enumClasses.foreach { enumClass =>
       enumClass.getEnumConstants.map{enum =>
         tmp(enum) = idx
         idx += 1
@@ -13,5 +15,7 @@ trait EnumUnionCompanion {
     }
     tmp.toMap
   }
+
   def nEnums = enumToIdx.values.max + 1
+  def getIdx[T <: Enum[T]: Union_A_B](x: T): Int = enumToIdx(x.asInstanceOf[Enum[_]])
 }
--- core/src/test/scala/com/quantifind/boxwood/EnumUnionFeatureSetTest.scala
+++ core/src/test/scala/com/quantifind/boxwood/EnumUnionFeatureSetTest.scala
@@ -6,21 +6,21 @@ import org.scalatest.matchers.ShouldMatchers
 class EnumUnionFeatureSetTest extends FunSuite with ShouldMatchers {
   test("enumUnionFeatureSet") {
     val f = new BaseFeatureSet with A_B_FeatureSet
-    f.nFeatures should be (Union_A_B.nEnums)
+    f.nFeatures should be (TestUnion_A_B.nEnums)
     f.enumStartIdx should be (0)
 
     val f2 = new BaseFeatureSet with A_B_FeatureSet with Gender
-    f2.nFeatures should be (Union_A_B.nEnums + 2)
+    f2.nFeatures should be (TestUnion_A_B.nEnums + 2)
     f2.genderStartIdx should be (0)
     f2.enumStartIdx should be (2)
 
     val f3 = new BaseFeatureSet with Age with A_B_FeatureSet
-    f3.nFeatures should be (Union_A_B.nEnums + 6)
+    f3.nFeatures should be (TestUnion_A_B.nEnums + 6)
     f3.enumStartIdx should be (0)
     f3.ageStartIdx should be (4)
   }
 }
 
 trait A_B_FeatureSet extends EnumUnionFeatureSet {
-  def enumUnion = Union_A_B //TODO has to be a def -- add this to gotchas in README
+  def enumUnion = TestUnion_A_B //TODO has to be a def -- add this to gotchas in README
 }
