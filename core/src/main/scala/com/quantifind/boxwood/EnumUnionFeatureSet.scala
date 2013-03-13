ec992f001a94d86c9d976e620e74875b51375234
Imran Rashid <imran@quantifind.com>
2013-03-12 21:39:57 -0700

    EnumUnionFeatureSets have a get(enum) now, w/ tests

--- core/src/main/scala/com/quantifind/boxwood/EnumUnionFeatureSet.scala
+++ core/src/main/scala/com/quantifind/boxwood/EnumUnionFeatureSet.scala
@@ -7,4 +7,6 @@ trait EnumUnionFeatureSet[E <: EnumUnion[Enum[_]]] extends FeatureSet {
     enumStartIdx = idx
     super.setOffsetIndex(idx + enumUnion.nEnums)
   }
+
+  def get[T <: Enum[T]](x: T)(implicit evidence: E with EnumUnion[T]) = enumStartIdx + enumUnion.getIdx(x)
 }
--- core/src/test/scala/com/quantifind/boxwood/EnumUnionFeatureSetTest.scala
+++ core/src/test/scala/com/quantifind/boxwood/EnumUnionFeatureSetTest.scala
@@ -5,19 +5,33 @@ import org.scalatest.matchers.ShouldMatchers
 
 class EnumUnionFeatureSetTest extends FunSuite with ShouldMatchers {
   test("enumUnionFeatureSet") {
-    val f = new BaseFeatureSet with A_B_FeatureSet
-    f.nFeatures should be (Union_A_B.nEnums)
-    f.enumStartIdx should be (0)
+    {
+      val f = new BaseFeatureSet with A_B_FeatureSet
+      f.nFeatures should be (Union_A_B.nEnums)
+      f.enumStartIdx should be (0)
+      f.get(A.Foo) should be (f.enumStartIdx + A.Foo.ordinal())
+      f.get(B.X) should be (f.enumStartIdx + B.X.ordinal() + A.values().length)
+    }
 
-    val f2 = new BaseFeatureSet with A_B_FeatureSet with Gender
-    f2.nFeatures should be (Union_A_B.nEnums + 2)
-    f2.genderStartIdx should be (0)
-    f2.enumStartIdx should be (2)
+    {
+      val f2 = new BaseFeatureSet with A_B_FeatureSet with Gender
+      f2.nFeatures should be (Union_A_B.nEnums + 2)
+      f2.genderStartIdx should be (0)
+      f2.enumStartIdx should be (2)
+      f2.get(A.Foo) should be (f2.enumStartIdx + A.Foo.ordinal())
+      f2.get(B.X) should be (f2.enumStartIdx + B.X.ordinal() + A.values().length)
+    }
 
-    val f3 = new BaseFeatureSet with Age with A_B_FeatureSet
-    f3.nFeatures should be (Union_A_B.nEnums + 6)
-    f3.enumStartIdx should be (0)
-    f3.ageStartIdx should be (4)
+    {
+      val f3 = new BaseFeatureSet with Age with A_B_FeatureSet
+      f3.nFeatures should be (Union_A_B.nEnums + 6)
+      f3.enumStartIdx should be (0)
+      f3.ageStartIdx should be (4)
+      f3.get(A.Foo) should be (f3.enumStartIdx + A.Foo.ordinal())
+      f3.get(B.X) should be (f3.enumStartIdx + B.X.ordinal() + A.values().length)
+      f3.baby should be (4)
+      f3.child should be (5)
+    }
   }
 }
 
